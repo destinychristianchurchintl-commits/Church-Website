cc7c3c5f81698a17e09d49da573c522882ee4910
Ukattah-Divine603 <ukattahdivine603@gmail.com>
2026-02-11 00:30:02 +0100

    Still on home

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./Home";
+import Welcome from "./Welcome";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/welcome" element={<Welcome />} />
     </Routes>
   );
 }
